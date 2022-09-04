0e76bf1004c11654280e104d0e5df38158d691ce
babluroy <bablu.roy@batchservice.com>
2022-09-05 00:41:01 +0530

    code updated

--- src/config.js
+++ src/config.js
@@ -1,18 +1,23 @@
 import firebase from 'firebase'
 import "firebase/storage"
 
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const database = firebase.database();
 
   const auth = firebase.auth();
 
   export const storage  = firebase.storage();
 
-  export {auth};
-
-  export {database};
+  export {auth, database};
