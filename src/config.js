567aee0300b3ee330f949c21701fb7af3c073890
babluroy <bablu.roy@batchservice.com>
2022-09-05 00:12:41 +0530

    firebase credentials removed

--- src/config.js
+++ src/config.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 import "firebase/storage"
 
 var firebaseConfig = {
-  apiKey: "AIzaSyA-y0kVsU-5iCaq1fQHs8brS8ocKQgXLwA",
-  authDomain: "react-real-estate-1b202.firebaseapp.com",
-  databaseURL: "https://react-real-estate-1b202-default-rtdb.firebaseio.com",
-  projectId: "react-real-estate-1b202",
-  storageBucket: "react-real-estate-1b202.appspot.com",
-  messagingSenderId: "346482817664",
-  appId: "1:346482817664:web:14ad5a53c0193625e70465"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
